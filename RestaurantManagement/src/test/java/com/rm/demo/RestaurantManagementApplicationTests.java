1c6c03192c70960c7ca6a72298d1324f17968ed5
HOME-PC\HOME <ntnghia8480@gmail.com>
2020-12-21 15:32:55 +0700

    create back-end

---
+++ RestaurantManagement/src/test/java/com/rm/demo/RestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
